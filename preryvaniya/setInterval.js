d48903ac5a320142cf2873d8489a975ee5e1451d
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2017-06-05 15:39:48 +0300

    Останавливаем скрипт через некоторое время

--- preryvaniya/setInterval.js
+++ preryvaniya/setInterval.js
@@ -1,7 +1,9 @@
 var elTime = document.getElementById('time');
 function showtime(object) {
   var date = new Date();
-  object.innerHTML = date.toTimeString().substr(0,8)
+  object.innerHTML = date.toTimeString().substr(0,8);
 }
 
-setInterval("showtime(elTime)", 1000);
+var handle = setInterval("showtime(elTime)", 1000);
+
+setTimeout("clearInterval(handle)", 10000);
